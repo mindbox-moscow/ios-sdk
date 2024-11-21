6adec7b44d04510e077591c7ac81b59e0397e633
Sergei Semko <28645140+justSmK@users.noreply.github.com>
2024-11-21 19:59:54 +0300

    Test

--- fastlane/Fastfile.swift
+++ fastlane/Fastfile.swift
@@ -17,20 +17,21 @@ class Fastfile: LaneFile {
 
     func unitTestLane() {
         desc("Run unit tests")
-        gym(
-            project: .userDefined(project), // Используем gym для сборки
-            scheme: "Mindbox",
-            clean: true,
-            xcargs: "CI=true CODE_SIGNING_ALLOWED=NO"
-        )
-        scan(
-            project: .userDefined(project),
-            scheme: "Mindbox",
-            onlyTesting: ["MindboxTests"],
-            clean: false,
-            xcodebuildFormatter: "xcpretty",
-            skipBuild: true,
-            xcargs: "CI=true CODE_SIGNING_ALLOWED=NO"
+        scan(project: .userDefined(project),
+             scheme: "Mindbox",
+             forceQuitSimulator: .userDefined(true),
+             resetSimulator: .userDefined(true),
+             prelaunchSimulator: .userDefined(true),
+             onlyTesting: ["MindboxTests"],
+             clean: true,
+             includeSimulatorLogs: true,
+             xcodebuildFormatter: "xcpretty",
+             disableConcurrentTesting: true,
+             skipBuild: true,
+             testWithoutBuilding: .userDefined(true),
+             xcargs: "CI=true CODE_SIGNING_ALLOWED=NO COMPILER_INDEX_STORE_ENABLE=NO SWIFT_COMPILATION_MODE=singlefile",
+             destination: "platform=iOS Simulator,name=iPhone SE (3rd generation),OS=18.1",
+             numberOfRetries: 3
         )
     }
 }
